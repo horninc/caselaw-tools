74582d0e660b88531ede0904e59917e18e15ae55
Kristaps Horns <k.horns@ideaspool.nl>
2020-01-13 17:33:14 +0100

    Add the new dependencies

--- caselaw-tools/__init__.py
+++ caselaw-tools/__init__.py
@@ -1,5 +1,5 @@
 # Let users know if they're missing any of our hard dependencies
-hard_dependencies = ("numpy", "pytz", "tqdm")
+hard_dependencies = ("numpy", "pytz", "tqdm", "pandas", "requests", "lxml", "urllib", "json", "time")
 missing_dependencies = []
 
 for dependency in hard_dependencies:
